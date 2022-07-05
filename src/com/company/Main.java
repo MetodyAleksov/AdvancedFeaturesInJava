3692ca1a977f5ec6a4f2ee3bc6f0ef3e929e609f
Metody Aleksov <teddy57603@gmail.com>
2022-07-05 15:04:00 +0300

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,25 @@
 package com.company;
 
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+        FileReader reader = null;
         try{
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
         }
         catch(Exception ex){
 
+        }
+        finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                }
+            }
         }
     }
 }
