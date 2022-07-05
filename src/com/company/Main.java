408f0ae377eb82d20a31bde77849aa21d5bebce8
Metody Aleksov <teddy57603@gmail.com>
2022-07-05 15:43:08 +0300

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        FileReader reader = null;
-        try{
-            reader = new FileReader("file.txt");
+        try (var reader = new FileReader("file.txt")){
         }
         catch(Exception ex){
 
-        }
-        finally {
-            if(reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                }
-            }
         }
     }
 }
